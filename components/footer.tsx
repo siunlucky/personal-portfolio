12fbc8c95978b4c1c2cb9272f0d05a8c057ab20f
siunlucky <faizelfahad2@gmail.com>
2023-08-08 21:51:26 +0700

    update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="px-4 mb-10 text-center text-gray-500">
       <small className="block mb-2 text-xs">
-        &copy; 2030 Ricardo. All rights reserved.
+        &copy; 2030 Faiz Elfahad Kurniawan. All rights reserved.
       </small>
       <p className="text-xs">
         <span className="font-semibold">About this website:</span> built with
